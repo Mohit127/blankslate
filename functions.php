27f1b04bc80db4118de88a99a81e193884f54c05
Mohit127 <mohitchawla@hotmail.co.in>
2015-01-07 12:51:56 +0530

    Update functions.php
    Made changes in 'blankslate_comments_number' function to address the 'Strict standards : Only variables should be passed by reference' message.

--- functions.php
+++ functions.php
@@ -58,7 +58,8 @@ function blankslate_comments_number( $count )
 {
 if ( !is_admin() ) {
 global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
+$get_comments = get_comments( 'status=approve&post_id=' . $id );
+$comments_by_type = separate_comments( $get_comments );
 return count( $comments_by_type['comment'] );
 } else {
 return $count;
